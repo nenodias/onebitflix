f5bbe685933b9a81e517f819f6012344b5884ee5
nenodias <horacio.dias92@gmail.com>
2021-05-29 16:16:47 -0300

    Adjusting the vuetify

---
+++ onebitflix/app/javascript/packs/app.vue
@@ -0,0 +1,49 @@
+<template>
+ <v-container grid-list-md text-xs-center>
+  <v-layout row wrap>
+   <v-flex xs6 offset-xs3>
+    <v-tabs icons-and-text centered dark color="cyan">
+     <v-tabs-slider color="yellow"></v-tabs-slider>
+     <v-tab href="#tab-1">
+       Recents
+       <v-icon>phone</v-icon>
+     </v-tab>
+     <v-tab href="#tab-2">
+       Favorites
+       <v-icon>favorite</v-icon>
+     </v-tab>
+     <v-tab href="#tab-3">
+       Nearby
+       <v-icon>account_box</v-icon>
+     </v-tab>
+     <v-tab-item
+       v-for="i in 3"
+       :key="i"
+       :id="'tab-' + i"
+     >
+       <v-card flat>
+        <v-card-text>{{ text }}</v-card-text>
+       </v-card>
+      </v-tab-item>
+     </v-tabs>
+    </v-flex>
+   </v-layout>
+ </v-container>
+</template>
+ 
+<script>
+ export default {
+  data () {
+   return {
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
+   }
+  }
+}
+</script>
+ 
+<style scoped>
+p {
+ font-size: 2em;
+ text-align: center;
+}
+</style>
